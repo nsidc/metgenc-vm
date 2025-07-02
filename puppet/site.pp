127e389c735b58718cbf92879d59e88c958c1236
Julia Collins <collinsj@nsidc.org>
2025-07-02 16:58:42 -0600

    refine puppet config

--- puppet/site.pp
+++ puppet/site.pp
@@ -4,7 +4,7 @@ $vault_host = 'vault.apps.int.nsidc.org'
 
 $project = 'metgenc'
 $package = 'nsidc-metgenc'
-$version = '1.7.0'
+$version = '1.6.1'
 
 # Location for running metgenc
 file {"/home/vagrant/${project}":
@@ -88,6 +88,7 @@ exec {'conda-init':
   command => 'conda init bash',
   path    => '/opt/miniconda/bin/:/bin/',
   user    => 'vagrant',
+  group   => 'vagrant',
   require => [File['.conda dir'],
               Nsidc_miniconda::Install['/opt/miniconda']],
 }
@@ -101,19 +102,44 @@ exec { 'install-python-3.12':
 }
 
 exec { 'install-venv':
-  command => "python -m venv /home/vagrant/${project}/.venv",
-  path    => '/opt/miniconda/bin/:/bin/',
+  command  => "python -m venv /home/vagrant/${project}/.venv",
+  path     => '/opt/miniconda/bin/:/bin/',
   provider => 'shell',
+  user     => 'vagrant',
+  group    => 'vagrant',
   # timeout     => 1800,
   require => Exec['install-python-3.12']
 }
 
+# Workaround for dependency conflicts between earthaccess and metgenc
+#exec { 'install-s3fs':
+#  command => ". activate && pip install s3fs==2024.10.0",
+#  path    => "/home/vagrant/${project}/.venv/bin/:/bin/",
+#  provider => 'shell',
+#  user     => 'vagrant',
+#  group    => 'vagrant',
+#  timeout     => 1800,
+#  require => Exec['install-venv']
+#}
+#
 exec { 'install-metgenc':
-  command => ". activate && pip install ${package}==${version}",
+  command => ". activate && pip install ${package}",
   path    => "/home/vagrant/${project}/.venv/bin/:/bin/",
   provider => 'shell',
+  user     => 'vagrant',
+  group    => 'vagrant',
   timeout     => 1800,
   require => Exec['install-venv']
+}
+
+exec { 'upgrade-metgenc':
+  command => ". activate && pip install --upgrade ${package}",
+  path    => "/home/vagrant/${project}/.venv/bin/:/bin/",
+  provider => 'shell',
+  user     => 'vagrant',
+  group    => 'vagrant',
+  timeout     => 1800,
+  require => Exec['install-metgenc']
 }
 
 package { 'awscli':
